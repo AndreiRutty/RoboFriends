ca7c59ff1611ada48b031cadd838834cc3c42aa2
Andrei Rutty <andreirutty.ar20@gmail.com>
2022-07-05 09:35:42 +0400

    initial commit

---
+++ src/components/SearchBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const SearchBox = ({ searchChange }) => {
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robot"
+        name=""
+        id=""
+        onChange={searchChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
