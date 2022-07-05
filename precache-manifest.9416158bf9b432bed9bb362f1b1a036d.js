53d7ad38799c2c42d7e20fa5d889bfec683c808e
Andrei Rutty <andreirutty.ar20@gmail.com>
2022-07-05 09:51:19 +0400

    Updates

---
+++ precache-manifest.9416158bf9b432bed9bb362f1b1a036d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "50bd9235c02921292d7d",
+    "url": "/Robofriends/static/css/main.adad2aa1.chunk.css"
+  },
+  {
+    "revision": "50bd9235c02921292d7d",
+    "url": "/Robofriends/static/js/main.50bd9235.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "bbb0bcf5a5c4ebb78565",
+    "url": "/Robofriends/static/css/2.6ceecad4.chunk.css"
+  },
+  {
+    "revision": "bbb0bcf5a5c4ebb78565",
+    "url": "/Robofriends/static/js/2.bbb0bcf5.chunk.js"
+  },
+  {
+    "revision": "baccc8f06f43294855f3",
+    "url": "/Robofriends/static/js/runtime~main.baccc8f0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6004e1bde441408ed466918481640e6a",
+    "url": "/Robofriends/index.html"
+  }
+];
